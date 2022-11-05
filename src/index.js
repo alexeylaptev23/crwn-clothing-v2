14fa0ccf55947756e0679f40a97d5e6a3524ea4d
alexeylaptev23 <alexeylaptev23@gmail.com>
2022-11-05 16:50:05 +0400

    15 - redux-persist and redux-devtools

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
-          <App />
+        <App />
       </BrowserRouter>
-    </Provider>
+    </PersistGate>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
