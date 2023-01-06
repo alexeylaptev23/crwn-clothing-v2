90fc2cada2261815fc9dbe47062b867e39c130fc
alexeylaptev23 <alexeylaptev23@gmail.com>
2023-01-06 17:36:12 +0500

    18.199 - typing category action creators

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,5 +1,7 @@
 import { CATEGORIES_ACTION_TYPES } from './category.types';
 
+import { CategoryAction } from './category.action';
+
 export const CATEGORY_INITIAL_STATE = {
   categories: [],
   isLoading: false,
@@ -8,16 +10,15 @@ export const CATEGORY_INITIAL_STATE = {
 
 export const categoriesReducer = (
   state = CATEGORY_INITIAL_STATE,
-  action = {}
+  action = {} as CategoryAction
 ) => {
-  const { type, payload } = action;
-  switch (type) {
+  switch (action.type) {
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
       return {...state, isLoading: true};
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, categories: payload, isLoading: false };
+      return { ...state, categories: action.payload, isLoading: false };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, error: payload, isLoading: false };
+      return { ...state, error: action.payload, isLoading: false };
     default:
       return state;
   }
